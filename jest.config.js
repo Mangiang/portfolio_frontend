7df2c31e5612ed470349b4620a90c729fcdfe7b5
Mangiang <mangiang@orange.fr>
2018-07-23 21:18:27 +0200

    - adding puppeteer - adding UI test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     verbose: true,
     setupFiles: ['./jest.setup.js'],
-    moduleFileExtensions: ['js', 'jsx'],
+    moduleFileExtensions: ['js', 'jsx', 'json'],
     transform: {
         '^.+\\.jsx?$': 'babel-jest',
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':

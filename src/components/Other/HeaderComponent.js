90c2b847df8bfb73efcd3e89f3168df81207b434
Mangiang <mangiang@orange.fr>
2018-07-26 17:36:12 +0200

    - Updating header styles

--- src/components/Other/HeaderComponent.js
+++ src/components/Other/HeaderComponent.js
@@ -2,9 +2,6 @@ import React, {Component} from 'react';
 import {hot} from 'react-hot-loader';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-
-import backgroundImage from '../../images/backgroundImage.jpg';
-import LazyHero from 'react-lazy-hero';
 import style from './HeaderComponent.css';
 
 type Props = {
@@ -19,12 +16,11 @@ export class HeaderComponent extends Component<Props> {
 
     render() {
         return (
-            <LazyHero imageSrc={backgroundImage} minHeight='200px' color={'#4F4943'} isCentered={false} opacity={0.5} parallaxOffset={50}>
-                <div id="title" className={style['centerName']}>
-                    <h2 className={style['title']}>Arthur Joly</h2>
-                    <div id="subTitle" className={style['subTitle']}>Computer science engineer student</div>
-                </div>
-            </LazyHero>
+            <div id="title" className={style['centerName']}>
+                <h2 className={style['title']}>Arthur Joly</h2>
+                <div id="subTitle" className={style['subTitle']}>Computer science engineer student</div>
+                <div className={style['divLine']}/>
+            </div>
         )
     }
 }

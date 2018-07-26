8852ca1f6c9974f67927e7a034c74ec93f6e5632
Mangiang <mangiang@orange.fr>
2018-07-26 18:15:06 +0200

    - Updating tests

--- __tests__/Components/HeaderComponent.test.js
+++ __tests__/Components/HeaderComponent.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import {shallow} from 'enzyme';
 import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk'
 
-import {HeaderComponent, HeaderComponentConnected} from '../../src/components/Other/HeaderComponent';
+import {HeaderComponent} from '../../src/components/Other/HeaderComponent';
 
 jest.mock('react-router-dom');
 
@@ -18,116 +16,5 @@ describe('>>> HEADER COMPONENT --- Snapshot', () => {
     it('+++ capturing Snapshot of Projects list dumb component', () => {
         const renderedValue = renderer.create(<HeaderComponent currentPage={currentPage} history={history} requestNavigation={requestNavigation}/>).toJSON();
         expect(renderedValue).toMatchSnapshot();
-    });
-});
-
-describe('>>> HEADER REDUX COMPONENT', () => {
-    const mockStore = configureStore(middlewares);
-
-    it('+++ render the component', () => {
-        const initialState = {navHeader: "projectsList"};
-
-        let store = mockStore(initialState);
-        store.dispatch = jest.fn();
-
-        let component = shallow(<HeaderComponentConnected store={store}/>);
-
-        expect(component).not.toBe(null);
-        expect(component).not.toBe(undefined);
-    });
-
-    describe('+++ Testing description', () => {
-        it('--- check description value on Projects list page', () => {
-            const initialState = {navHeader: "projectsList"};
-            const expectedValue = "Here is a list of my projects";
-
-            let store = mockStore(initialState);
-            let wrapper = shallow(<HeaderComponentConnected store={store}/>);
-            let component = wrapper.dive().find('h4');
-
-            expect(component.text()).toEqual(expectedValue);
-        });
-
-        it('--- check description value on Timeline page', () => {
-            const initialState = {navHeader: "timeline"};
-            const expectedValue = "Here are both my professional and school my experience";
-
-            let store = mockStore(initialState);
-            let wrapper = shallow(<HeaderComponentConnected store={store}/>);
-            let component = wrapper.dive().find('h4');
-
-            expect(component.text()).toEqual(expectedValue);
-        });
-    });
-
-    describe('+++ Testing button text', () => {
-        it('--- check description value on Projects list page', () => {
-            const initialState = {navHeader: "projectsList"};
-            const expectedValue = "Timeline";
-
-            let store = mockStore(initialState);
-            let wrapper = shallow(<HeaderComponentConnected store={store}/>);
-            let component = wrapper.dive().find('button');
-
-            expect(component.text()).toEqual(expectedValue);
-        });
-
-        it('--- check description value on Timeline page', () => {
-            const initialState = {navHeader: "timeline"};
-            const expectedValue = "Projects list";
-
-            let store = mockStore(initialState);
-            let wrapper = shallow(<HeaderComponentConnected store={store}/>);
-            let component = wrapper.dive().find('button');
-
-            expect(component.text()).toEqual(expectedValue);
-        });
-    });
-
-    describe('+++ Testing mapStateToProps', () => {
-        it('--- check currentPage default value without navHeader', () => {
-            const initialState = {};
-
-            let store = mockStore(initialState);
-            let component = shallow(<HeaderComponentConnected store={store}/>);
-
-            expect(component.prop('currentPage')).toEqual('projectsList');
-        });
-
-        it('--- check currentPage default value with empty navHeader string', () => {
-            const initialState = {navHeader: ""};
-
-            let store = mockStore(initialState);
-            let component = shallow(<HeaderComponentConnected store={store}/>);
-
-            expect(component.prop('currentPage')).toEqual('projectsList');
-        });
-
-        it('--- check currentPage default value with random navHeader string', () => {
-            const initialState = {navHeader: "azepojazoiejapoizeh"};
-
-            let store = mockStore(initialState);
-            let component = shallow(<HeaderComponentConnected store={store}/>);
-
-            expect(component.prop('currentPage')).toEqual('projectsList');
-        });
-
-        it('--- check currentPage value setting projectsList', () => {
-            const initialState = {navHeader: "projectsList"};
-
-            let store = mockStore(initialState);
-            let component = shallow(<HeaderComponentConnected store={store}/>);
-
-            expect(component.prop('currentPage')).toEqual(initialState.navHeader);
-        });
-
-        it('--- check currentPage value setting timeline', () => {
-            const initialState = {navHeader: "timeline"};
-
-            let store = mockStore(initialState);
-            let component = shallow(<HeaderComponentConnected store={store}/>);
-
-            expect(component.prop('currentPage')).toEqual(initialState.navHeader);
-        });
     });
 });
--- __tests__/UI/HeaderComponentUI.test.js
+++ __tests__/UI/HeaderComponentUI.test.js
@@ -29,34 +29,9 @@ describe('>>> HEADER COMPONENT', () => {
     });
 
     test('Check the page title', async () => {
-        await page.waitForSelector('.sc-bdVaJa');
-
         const pageTitle = await page.title();
 
         expect(pageTitle).toBe("Arthur Joly");
-    });
-
-
-    test('Check the background image name', async () => {
-        await page.waitForSelector('.sc-bdVaJa');
-
-        let backgroundImage = await page.$eval('.sc-bdVaJa', e => e.getAttribute("src"));
-
-        expect(backgroundImage.startsWith("/static/media/backgroundImage")).toBe(true);
-    }, 16000);
-
-    test('Check the background image extension', async () => {
-        let backgroundImage = await page.$eval('.sc-bdVaJa', e => e.getAttribute("src"));
-        expect(backgroundImage.endsWith(".jpg")).toBe(true);
-    }, 16000);
-
-
-    test('Check the title text', async () => {
-        await page.waitForSelector('#title');
-
-        let titleVal = await page.$eval('#title', e => e.innerHTML);
-
-        expect(titleVal).toBe("Arthur Joly");
     });
 
     afterAll(async (done) => {
